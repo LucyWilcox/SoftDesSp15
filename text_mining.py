8c38a7a41e38091f8426a76ceed9fbc6e33352d3
Lucy Wilcox <lucy.wilcox@students.olin.edu>
2015-02-23 16:59:01 -0500

    working text mining

--- text_mining.py
+++ text_mining.py
@@ -6,11 +6,13 @@ import string
 from pattern.web import *
 from pattern.en import *
 
-#the_picture_of_grey = URL('https://www.gutenberg.org/cache/epub/174/pg174.txt').download()
-the_picture_of_grey = URL('http://www.gutenberg.lib.md.us/1/7/174/174.txt').download()
-#the_picture_of_grey = open('kamasutra.txt')
-#print the_picture_of_grey
-#print len(the_picture_of_grey)
+the_picture_of_dorian_grey = URL('http://www.gutenberg.lib.md.us/1/7/174/174.txt').download()
+the_yellow_wallpaper = URL('http://www.gutenberg.lib.md.us/1/9/5/1952/1952.txt').download()
+frankenstein = URL('http://www.gutenberg.lib.md.us/8/84/84.txt').download()
+turn_of_the_screw = URL('http://www.gutenberg.lib.md.us/2/0/209/209.txt').download()
+dracula = URL('http://www.gutenberg.lib.md.us/3/4/345/345.txt').download()
+book = dracula
+
 #print sentiment(the_picture_of_grey)
 # def text_processing(book):
 #     all_words = []
@@ -20,36 +22,70 @@ the_picture_of_grey = URL('http://www.gutenberg.lib.md.us/1/7/174/174.txt').down
 #     return all_words
 
 def book_to_list(book):
+    """
+
+    """
     words = book.split()
-    all_words = []
-    for word in words:
-        all_words.append(word)
-    return all_words
-# allwords = [word for word in words]
+    return words
 
 
-def strip_book(all_words):
-    indices = [i for i, s in enumerate(all_words) if '***' in s]
-    print indices[1]
-    print indices[2]
-    loop_len = len(all_words)
-    i = 0
-    #print all_words
-    for i in range(0, 2):
-        print i
-        all_words.pop(i)
-        print all_words[i]
-        print all_words
-    # for i in range(indices[2], len(all_words)- (loop_len - indices[2])):
-    #     all_words.pop(i)
-    #     i += 1
-    return all_words
+def remove_gute(book):
+    """Removes header and footer from project gutenberg books.
+    book: list of strings
+    returns: 
+    """
+    indices = [i for i, s in enumerate(book) if '***' in s]
+    start = indices[1] + 1
+    end = indices[2]  
+    book = book[start:end]
+    return book
+ 
 
-#the_picture_of_grey = text_processing(the_picture_of_grey)
-all_words = book_to_list(the_picture_of_grey)
-words = strip_book(all_words)
-#print all_words
-#print words
+def split_to_parts(book):
+    """
+
+    """
+    segment_len = len(book) / 5
+    segment_list = []
+    for i in range(0, 5):
+        segment_list.append(book[i * segment_len: (i + 1) * segment_len])
+
+    return segment_list
+
+    # segment_1 = book[0:segment_len]
+    # segment_2 = book[segment_len: 2 * segment_len]
+    # segment_3 = book[2 * segment_len: 3 * segment_len]
+    # segment_4 = book[3 * segment_len: segment_len *4]
+    # segment_5 = book[4* segment_len:]
+
+
+def group_to_string(list_of_words):
+    """
+
+    """
+    book_strings = []
+    for i in range(0, 5):
+        book_strings.append(" ".join(list_of_words[i]))
+
+    return book_strings
+
+def get_sentiment(five_strings):
+    """
+
+    """
+    sentiment_list = []
+    for i in range(0, 5):
+        sentiment_list.append(sentiment(five_strings[i]))
+    sentiment_list =  [x[0] for x in sentiment_list]
+    return sentiment_list
+
+
+all_words = book_to_list(book)
+words = remove_gute(all_words)
+five_lists = split_to_parts(words)
+five_strings = group_to_string(five_lists)
+sentiments = get_sentiment(five_strings)
+print sentiments
 
 #names = open('class_names.txt')
 # name_sentiment = []
--- toolbox/word_frequency_analysis/frequency.py
+++ toolbox/word_frequency_analysis/frequency.py
@@ -1,24 +1,50 @@
 """ Analyzes the word frequencies in a book downloaded from
-	Project Gutenberg """
+    Project Gutenberg """
 
 import string
+from pattern.web import *
+from pattern.en import *
+book = URL('http://www.gutenberg.lib.md.us/1/7/174/174.txt').download()
+
 
 def get_word_list(file_name):
-	""" Reads the specified project Gutenberg book.  Header comments,
-		punctuation, and whitespace are stripped away.  The function
-		returns a list of the words used in the book as a list.
-		All words are converted to lower case.
-	"""
-	pass
+    """ Reads the specified project Gutenberg book.  Header comments,
+        punctuation, and whitespace are stripped away.  The function
+        returns a list of the words used in the book as a list.
+        All words are converted to lower case.
+    """
+    f = open(file_name,'r')
+    prnc= string.punctuation.split()
+    punc =  [i for i, s in prnc]
+    print punc
+    lines = f.readlines()
+    curr_line = 0
+    while lines[curr_line].find('START OF THIS PROJECT GUTENBERG EBOOK') == -1:
+        curr_line += 1
+    lines = lines[curr_line+1:]
+
+    for line in lines:
+        words = string.split(line)
+        #print words
+        # for word in words:
+        #     if word in punc:
+
+            # for c in word:
+            #     c.replace(string.punctuation, "")
+
+    return words
+    pass
 
 def get_top_n_words(word_list, n):
-	""" Takes a list of words as input and returns a list of the n most frequently
-		occurring words ordered from most to least frequently occurring.
+    """ Takes a list of words as input and returns a list of the n most frequently
+        occurring words ordered from most to least frequently occurring.
 
-		word_list: a list of words (assumed to all be in lower case with no
-					punctuation
-		n: the number of words to return
-		returns: a list of n most frequently occurring words ordered from most
-				 frequently to least frequentlyoccurring
-	"""
-	pass
+        word_list: a list of words (assumed to all be in lower case with no
+                    punctuation
+        n: the number of words to return
+        returns: a list of n most frequently occurring words ordered from most
+                 frequently to least frequentlyoccurring
+    """
+    pass
+
+print get_word_list("kamasutra.txt")
