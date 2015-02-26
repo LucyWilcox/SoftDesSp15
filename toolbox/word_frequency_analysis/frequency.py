eba75dd4b126b18fa6b5aefe0f268454e0201fdf
Lucy Wilcox <lucy.wilcox@students.olin.edu>
2015-02-26 12:23:28 -0500

    adding frequency analysis

--- toolbox/word_frequency_analysis/frequency.py
+++ toolbox/word_frequency_analysis/frequency.py
@@ -2,10 +2,6 @@
     Project Gutenberg """
 
 import string
-from pattern.web import *
-from pattern.en import *
-book = URL('http://www.gutenberg.lib.md.us/1/7/174/174.txt').download()
-
 
 def get_word_list(file_name):
     """ Reads the specified project Gutenberg book.  Header comments,
@@ -14,9 +10,9 @@ def get_word_list(file_name):
         All words are converted to lower case.
     """
     f = open(file_name,'r')
-    prnc= string.punctuation.split()
-    punc =  [i for i, s in prnc]
-    print punc
+    punctuation = string.punctuation
+    count = dict()
+    all_words = []
     lines = f.readlines()
     curr_line = 0
     while lines[curr_line].find('START OF THIS PROJECT GUTENBERG EBOOK') == -1:
@@ -24,16 +20,17 @@ def get_word_list(file_name):
     lines = lines[curr_line+1:]
 
     for line in lines:
-        words = string.split(line)
-        #print words
-        # for word in words:
-        #     if word in punc:
+        words_in_line = string.split(line)
+        for word in words_in_line:
+            word = word.lower()
+            for char in punctuation:
+                word = word.replace(char, "")
+            if word not in count:
+                count[word] = 1
+            else:
+                count[word] += 1
+    return count
 
-            # for c in word:
-            #     c.replace(string.punctuation, "")
-
-    return words
-    pass
 
 def get_top_n_words(word_list, n):
     """ Takes a list of words as input and returns a list of the n most frequently
@@ -45,6 +42,9 @@ def get_top_n_words(word_list, n):
         returns: a list of n most frequently occurring words ordered from most
                  frequently to least frequentlyoccurring
     """
-    pass
+    sort = sorted(word_list, key = word_list.get, reverse = True)
+    return sort[:n]
 
-print get_word_list("kamasutra.txt")
+
+counts = get_word_list("kamasutra.txt")
+print get_top_n_words(counts, 100)
