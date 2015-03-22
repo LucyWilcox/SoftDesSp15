f978d7b4da21b0ac824f992f98dabaee4fa1db44
Lucy Wilcox <lucy.wilcox@students.olin.edu>
2015-03-21 20:20:07 -0400

    progress on ml toolbox

--- toolbox/ml/learning_curve.py
+++ toolbox/ml/learning_curve.py
@@ -9,12 +9,6 @@ from sklearn.linear_model import LogisticRegression
 data = load_digits()
 #print data.DESCR
 
-X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, train_size=0.5)
-model = LogisticRegression(C=10**-10)
-model.fit(X_train, y_train)
-print "Train accuracy %f" %model.score(X_train,y_train)
-print "Test accuracy %f"%model.score(X_test,y_test)
-
 num_trials = 10
 train_percentages = range(5,95,5)
 test_accuracies = numpy.zeros(len(train_percentages))
@@ -24,10 +18,9 @@ test_accuracies = numpy.zeros(len(train_percentages))
 # You should repeat each training percentage num_trials times to smooth out variability
 # for consistency with the previous example use model = LogisticRegression(C=10**-10) for your learner
 
-# TODO: your code here
-
 digits = load_digits()
 #print digits.DESCR
+
 fig = plt.figure()
 for i in range(10):
     subplot = fig.add_subplot(5,2,i+1)
@@ -35,8 +28,21 @@ for i in range(10):
 
 plt.show()
 
+for size in train_percentages:
+	accumulated_accuracy = 0.0
+	for i in range(num_trials):
+		X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, train_size= size)
+		model = LogisticRegression(C=10**-10)
+		model.fit(X_train, y_train)
+		accumulated_accuracy += model.score(X_test, y_test)
+	#print "Train accuracy %f" %model.score(X_train,y_train)
+	#print "Test accuracy %f"%model.score(X_test,y_test)
+	test_accuracies[size/5.0 - 1] = accumulated_accuracy/num_trials
+
 
 fig = plt.figure()
+#print train_percentages
+#print test_accuracies
 plt.plot(train_percentages, test_accuracies)
 plt.xlabel('Percentage of Data Used for Training')
 plt.ylabel('Accuracy on Test Set')
